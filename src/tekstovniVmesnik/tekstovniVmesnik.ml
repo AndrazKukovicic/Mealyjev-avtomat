73a3c220812d1a32c0f8bf7a8abb8a5c26657225
Andraž Kukovičič <andraz.kukovicic@gmail.com>
2024-08-10 01:50:08 +0200

    vmesnnik popravljam

--- src/tekstovniVmesnik/tekstovniVmesnik.ml
+++ src/tekstovniVmesnik/tekstovniVmesnik.ml
@@ -23,18 +23,48 @@ type msg =
   | VrniVPrvotnoStanje
   | SestaviNovMealyjevAvtomat
 
+let init avtomat =
+  {
+    avtomat;
+    stanje_avtomata = ZagnaniAvtomat.pozeni avtomat (Trak.prazen);
+    stanje_vmesnika = SeznamMoznosti;
+    prebrani_izhod = ""
+  }
+
+
+let ustvari_avtomat_interaktivno () =
+  print_endline "Vnesite Število stanj: ";
+  let st_stanj = int_of_string (read_line ()) in 
+  let rec ustvari_stanja n acc =
+    if n <= 0 then acc
+    else
+      let stanje = Printf.sprintf "q%d" (st_stanj - n + 1) in
+      ustvari_stanja (n - 1) (stanje :: acc) in
+      let stanja = ustvari_stanja st_stanj [] in
+      print_endline "Vnesite prehode v formatu (tr stanje, vhod, nasl stanje, izhod): ";
+      let rec preberi_prehode () = 
+        print_string "> ";
+        match read_line () with
+        | "" -> []
+        | prehod -> 
+          let parts = String.split_on_char ',' prehod in 
+          let prehod = (List.nth parts 0, List.nth parts 1, List.nth parts 2, List.nth parts 3) in
+          prehod :: preberi_prehode () in
+
+        let prehodna_funkcija = preberi_prehode () in
+        ustvari_avtomat stanja prehodna_funkcija
 let preberi_niz model niz =
   let rec aux zagnani_avtomat i =
     if i < String.length niz then
       let zagnani_avtomat = korak_naprej zagnani_avtomat in
-      aux zagnani_avtomat (i + 1)
+      aux (Option.to_result zagnani_avtomat) (i + 1)
     else zagnani_avtomat
   in
   let zagnani_avtomat = aux model.stanje_avtomata 0 in
   let prebrani_izhod = zagnani_avtomat.trak.izhod in
   { model with stanje_avtomata = zagnani_avtomat; prebrani_izhod }
 
-let update model = function
+let rec update model = function
   | PreberiNiz str ->
       let nov_model = preberi_niz model str in
       { nov_model with stanje_vmesnika = RezultatPrebranegaNiza }
@@ -47,9 +77,10 @@ let update model = function
         prebrani_izhod = ""
       }
   | SestaviNovMealyjevAvtomat ->
-      let avtomat = ustvari_avtomat "" [] [] in
-      let model = init avtomat in
+      let model = init (ustvari_avtomat_interaktivno ()) in
       { model with stanje_vmesnika = SestaviNovAvtomat }
+
+
 
 let rec izpisi_moznosti () =
   print_endline "1) Vnesi in preberi niz.";
@@ -57,8 +88,8 @@ let rec izpisi_moznosti () =
   print_string "> ";
   match read_line () with
   
-  | "2" -> ZamenjajVmesnik BranjeNiza
-  | "3" -> SestaviNovMealyjevAvtomat
+  | "1" -> ZamenjajVmesnik BranjeNiza
+  | "2" -> SestaviNovMealyjevAvtomat
   | _ ->
       print_endline "** VNESI 1 ALI 2 **";
       izpisi_moznosti ()
@@ -82,16 +113,8 @@ let view model =
       print_endline "Niz ni veljaven";
       ZamenjajVmesnik SeznamMoznosti
   | SestaviNovAvtomat ->
-      main ();
       ZamenjajVmesnik SeznamMoznosti
 
-let init avtomat =
-  {
-    avtomat;
-    stanje_avtomata = ZagnaniAvtomat.pozeni avtomat (Trak.prazen);
-    stanje_vmesnika = SeznamMoznosti;
-    prebrani_izhod = ""
-  }
 
 let rec loop model =
   let msg = view model in
